fe9e4fff235570e5bd4b8cec00e3a80cedc11191
Ali Mehralian <alimehralian.info@gmail.com>
2022-08-15 12:43:36 +0430

    finish home page

--- src/js/js.js
+++ src/js/js.js
@@ -1,11 +1,11 @@
 // auto check theme
-// if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-//     document.getElementById("moon_mode").style.display = "none";
-//     document.getElementById("sun_mode").style.display = "block";
-// } else {
-//     document.getElementById("moon_mode").style.display = "block";
-//     document.getElementById("sun_mode").style.display = "none";
-// }
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.getElementById("moon_mode").style.display = "none";
+    document.getElementById("sun_mode").style.display = "block";
+} else {
+    document.getElementById("moon_mode").style.display = "block";
+    document.getElementById("sun_mode").style.display = "none";
+}
 
 function dark_switch() {
     
