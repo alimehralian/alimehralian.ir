9e7d050afb3cff4bbf1428d5ea83d82d919efecf
mrx <mrx@mrxs-MacBook-Air.local>
2023-08-09 18:43:16 +0330

    fa lang

--- src/js/js.js
+++ src/js/js.js
@@ -26,3 +26,22 @@ function dark_switch() {
 a = Date.now();
 console.log(new Date(a));
 
+
+$(document).ready(function () {
+
+    // Show dropdown
+    $('.selected').click(function () {
+        $('.custom-sel').addClass('show-sel');
+        $('.custom-sel a').removeClass('hidden');
+    });
+
+    // Hide dropdown when not focused
+    $('.custom-sel').focusout(function () {
+        $('.custom-sel').removeClass('show-sel');
+        $('.custom-sel a:not(:first)').addClass('hidden');
+    }).blur(function () {
+        $('.custom-sel').removeClass('show-sel');
+        $('.custom-sel a:not(:first)').addClass('hidden');
+    });
+
+});
