e981903490ee2da47e8e363f7efdf00554074930
mrx <mrx@mrxs-MacBook-Air.local>
2023-08-09 17:09:47 +0330

    dark mood

--- src/js/js.js
+++ src/js/js.js
@@ -1,31 +1,26 @@
-// // auto check theme
-// if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-//     document.getElementById("moon_mode").style.display = "none";
-//     document.getElementById("sun_mode").style.display = "block";
-// } else {
-//     document.getElementById("moon_mode").style.display = "block";
-//     document.getElementById("sun_mode").style.display = "none";
-// }
+// auto check theme
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.getElementById("moon_mode").style.display = "none";
+    document.getElementById("sun_mode").style.display = "block";
+} else {
+    document.getElementById("moon_mode").style.display = "block";
+    document.getElementById("sun_mode").style.display = "none";
+}
 
 function dark_switch() {
-    
+
     if(document.documentElement.classList.contains("dark")){
         localStorage.theme = "light";
-        document.documentElement.classList.toggle("light");
-        document.documentElement.classList.add('dark');
+        document.documentElement.classList.toggle("dark");
 
     }else{
         localStorage.theme = "dark";
         document.documentElement.classList.toggle("dark");
-        document.documentElement.classList.remove('dark')
-
     }
-
-    location.reload();
 
 }
 
 
-// a = Date.now();
-// console.log(new Date(a));
+a = Date.now();
+console.log(new Date(a));
 
