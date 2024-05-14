5dd03849d24fa7a3e961e11bd38b3b81bc0b5319
mrx <alimehralian.info@gmail.com>
2024-05-14 16:15:56 +0330

    edit image

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         'my-yellow' : '#aa00ff',
       },
       backgroundImage: {
-        'alimehralian': "url('/src/images/alimehralian3.jpg')",
+        'alimehralian': "url('/src/images/alimh.jpg')",
       },
       height: {
         '100vh-80': 'calc(100vh - 80px)',
